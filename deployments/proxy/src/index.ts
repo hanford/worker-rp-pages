0e9a92aa80abdca190aa8775465831317d33c7e0
Jack Hanford <jackhanford@gmail.com>
2022-08-02 09:55:36 -0700

    worker

--- deployments/proxy/src/index.ts
+++ deployments/proxy/src/index.ts
@@ -3,39 +3,43 @@ import useReflare from "reflare";
 const handleRequest = async (request: Request): Promise<Response> => {
   const reflare = await useReflare();
 
+  const isLocalRequest = request.url.includes("localhost");
+
+  console.log(request.url);
+
   reflare.push({
     path: "/order*",
     upstream: {
-      domain: "localhost",
-      protocol: "http",
-      port: 4999,
+      domain: isLocalRequest ? "localhost" : "order.jackhanford.com",
+      protocol: isLocalRequest ? "http" : "https",
+      port: isLocalRequest ? 4999 : undefined,
     },
   });
 
   reflare.push({
     path: "/admin*",
     upstream: {
-      domain: "localhost",
-      protocol: "http",
-      port: 4997,
+      domain: isLocalRequest ? "localhost" : "admin.jackhanford.com",
+      protocol: isLocalRequest ? "http" : "https",
+      port: isLocalRequest ? 4997 : undefined,
     },
   });
 
   reflare.push({
     path: "/checkout*",
     upstream: {
-      domain: "localhost",
-      protocol: "http",
-      port: 4998,
+      domain: isLocalRequest ? "localhost" : "checkout.jackhanford.com",
+      protocol: isLocalRequest ? "http" : "https",
+      port: isLocalRequest ? 4998 : undefined,
     },
   });
 
   reflare.push({
     path: "/*",
     upstream: {
-      domain: "localhost",
-      protocol: "http",
-      port: 4996,
+      domain: isLocalRequest ? "localhost" : "menu.jackhanford.com",
+      protocol: isLocalRequest ? "http" : "https",
+      port: isLocalRequest ? 4996 : undefined,
     },
   });
 
