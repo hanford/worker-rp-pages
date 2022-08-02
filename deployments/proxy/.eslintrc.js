d47afec9e9c24018d20ab45e97351bafaa0e0e10
Jack Hanford <jackhanford@gmail.com>
2022-08-02 14:02:16 -0700

    another try

--- deployments/proxy/.eslintrc.js
+++ deployments/proxy/.eslintrc.js
@@ -3,16 +3,11 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ["@typescript-eslint"],
 };
--- deployments/proxy/src/index.ts
+++ deployments/proxy/src/index.ts
@@ -1,51 +1,109 @@
-import useReflare from "reflare";
+// import useReflare from "reflare";
 
-const handleRequest = async (request: Request): Promise<Response> => {
-  const reflare = await useReflare();
+// const handleRequest = async (request: Request): Promise<Response> => {
+//   const reflare = await useReflare();
 
-  const isLocalRequest = request.url.includes("localhost");
+//   const isLocalRequest = request.url.includes("localhost");
 
-  console.log(request.url);
+//   console.log(request.url);
 
-  reflare.push({
-    path: "/order*",
-    upstream: {
-      domain: isLocalRequest ? "localhost" : "order.jackhanford.com",
-      protocol: isLocalRequest ? "http" : "https",
-      port: isLocalRequest ? 4999 : undefined,
-    },
-  });
+//   reflare.push({
+//     path: "/order*",
+//     upstream: {
+//       domain: isLocalRequest ? "localhost" : "order.jackhanford.com",
+//       protocol: isLocalRequest ? "http" : "https",
+//       port: isLocalRequest ? 4999 : undefined,
+//     },
+//   });
 
-  reflare.push({
-    path: "/admin*",
-    upstream: {
-      domain: isLocalRequest ? "localhost" : "admin.jackhanford.com",
-      protocol: isLocalRequest ? "http" : "https",
-      port: isLocalRequest ? 4997 : undefined,
-    },
-  });
+//   reflare.push({
+//     path: "/admin",
+//     upstream: {
+//       domain: isLocalRequest ? "localhost" : "admin-mfe.pages.dev",
+//       protocol: isLocalRequest ? "http" : "https",
+//       port: isLocalRequest ? 4997 : 80,
+//     },
+//   });
 
-  reflare.push({
-    path: "/checkout*",
-    upstream: {
-      domain: isLocalRequest ? "localhost" : "checkout.jackhanford.com",
-      protocol: isLocalRequest ? "http" : "https",
-      port: isLocalRequest ? 4998 : undefined,
-    },
-  });
+//   reflare.push({
+//     path: "/checkout*",
+//     upstream: {
+//       domain: isLocalRequest ? "localhost" : "checkout.jackhanford.com",
+//       protocol: isLocalRequest ? "http" : "https",
+//       port: isLocalRequest ? 4998 : undefined,
+//     },
+//   });
 
-  reflare.push({
-    path: "/*",
-    upstream: {
-      domain: isLocalRequest ? "localhost" : "menu.jackhanford.com",
-      protocol: isLocalRequest ? "http" : "https",
-      port: isLocalRequest ? 4996 : undefined,
-    },
-  });
+//   reflare.push({
+//     path: "/*",
+//     upstream: {
+//       domain: isLocalRequest ? "localhost" : "menu.jackhanford.com",
+//       protocol: isLocalRequest ? "http" : "https",
+//       port: isLocalRequest ? 4996 : undefined,
+//     },
+//   });
 
-  return reflare.handle(request);
-};
+//   return reflare.handle(request);
+// };
+
+// addEventListener("fetch", (event) => {
+//   event.respondWith(handleRequest(event.request));
+// });
 
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
+
+const map = {
+  order: {
+    dev: "localhost",
+    port: "4999",
+    prod: "https://order.jackhanford.com",
+  },
+  checkout: {
+    dev: "localhost",
+    port: "4998",
+    prod: "https://checkout.jackhanford.com",
+  },
+  admin: {
+    dev: "localhost",
+    port: "4997",
+    prod: "https://admin.jackhanford.com",
+  },
+  menu: {
+    dev: "localhost",
+    port: "4996",
+    prod: "https://menu.jackhanford.com",
+  },
+};
+
+async function handleRequest(request: Request) {
+  const url = new URL(request.url);
+
+  const { pathname, hostname } = url;
+
+  const isLocal = hostname.includes("localhost");
+  const envKey = isLocal ? "dev" : "prod";
+  let destination = "menu";
+
+  if (pathname.startsWith("/admin")) {
+    destination = "admin";
+  } else if (pathname.startsWith("/checkout")) {
+    destination = "checkout";
+  } else if (pathname.startsWith("/order")) {
+    destination = "order";
+  }
+
+  if (isLocal) {
+    url.port = map[destination].port;
+  } else {
+    url.pathname = url.pathname.replace(`/${destination}`, "");
+  }
+
+  url.hostname = map[destination][envKey];
+
+  // eslint-disable-next-line
+  // @ts-ignore
+  const response = await fetch(url, request);
+  return response;
+}
