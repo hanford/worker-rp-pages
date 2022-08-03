eb23e11a72d489db49ad90b5aa56ab0be25a77ff
Jack Hanford <jackhanford@gmail.com>
2022-08-02 17:09:32 -0700

    Miniflare and Reflare fork (#3)

---
+++ deployments/proxy/src/index.ts
@@ -0,0 +1,123 @@
+import useReflare from "./reflare";
+
+const handleRequest = async (request: Request): Promise<Response> => {
+  const reflare = await useReflare();
+
+  const isLocalRequest = request.url.includes("localhost");
+
+  console.log(request.url);
+
+  reflare.push({
+    path: "/order*",
+    upstream: {
+      domain: isLocalRequest ? "localhost" : "order.jackhanford.com",
+      protocol: isLocalRequest ? undefined : "https",
+      port: isLocalRequest ? 4999 : undefined,
+      pathRewrite: (path) => {
+        if (isLocalRequest) return path;
+        return path.replace("/order", "");
+      },
+    },
+  });
+
+  reflare.push({
+    path: "/admin*",
+    upstream: {
+      domain: isLocalRequest ? "localhost" : "admin.jackhanford.com",
+      protocol: isLocalRequest ? undefined : "https",
+      port: isLocalRequest ? 4997 : 80,
+      pathRewrite: (path) => {
+        if (isLocalRequest) return path;
+        return path.replace("/admin", "");
+      },
+    },
+  });
+
+  reflare.push({
+    path: "/checkout*",
+    upstream: {
+      domain: isLocalRequest ? "localhost" : "checkout.jackhanford.com",
+      protocol: isLocalRequest ? undefined : "https",
+      port: isLocalRequest ? 4998 : undefined,
+      pathRewrite: (path) => {
+        if (isLocalRequest) return path;
+        return path.replace("/checkout", "");
+      },
+    },
+  });
+
+  reflare.push({
+    path: "/*",
+    upstream: {
+      domain: isLocalRequest ? "localhost" : "menu.jackhanford.com",
+      protocol: isLocalRequest ? "http" : "https",
+      port: isLocalRequest ? 4996 : undefined,
+    },
+  });
+
+  return reflare.handle(request);
+};
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+// addEventListener("fetch", (event) => {
+//   event.respondWith(handleRequest(event.request));
+// });
+
+// const map = {
+//   order: {
+//     dev: "localhost",
+//     port: "4999",
+//     prod: "https://order.jackhanford.com",
+//   },
+//   checkout: {
+//     dev: "localhost",
+//     port: "4998",
+//     prod: "https://checkout.jackhanford.com",
+//   },
+//   admin: {
+//     dev: "localhost",
+//     port: "4997",
+//     prod: "https://admin.jackhanford.com",
+//   },
+//   menu: {
+//     dev: "localhost",
+//     port: "4996",
+//     prod: "https://menu.jackhanford.com",
+//   },
+// };
+
+// async function handleRequest(request: Request) {
+//   const url = new URL(request.url);
+
+//   const { pathname, hostname } = url;
+
+//   const isLocal = hostname.includes("localhost");
+//   const envKey = isLocal ? "dev" : "prod";
+//   let destination = "menu";
+
+//   if (pathname.startsWith("/admin")) {
+//     destination = "admin";
+//   } else if (pathname.startsWith("/checkout")) {
+//     destination = "checkout";
+//   } else if (pathname.startsWith("/order")) {
+//     destination = "order";
+//   }
+
+//   if (isLocal) {
+//     url.port = map[destination].port;
+//   } else {
+//     url.pathname = url.pathname.replace(`/${destination}`, "");
+//   }
+
+//   url.hostname = map[destination][envKey];
+
+//   console.log({ url });
+
+//   // eslint-disable-next-line
+//   // @ts-ignore
+//   const response = await fetch(url, request);
+//   return response;
+// }
---
+++ deployments/proxy/src/reflare/utils.ts
@@ -0,0 +1,33 @@
+import { UpstreamOptions } from "./types";
+
+export const createResponse = (body: string, status: number): Response =>
+  new Response(body, {
+    status,
+  });
+
+export const getHostname = (request: Request): string => {
+  const url = new URL(request.url);
+  return url.host;
+};
+
+export const isSameOrigin = (url: URL, upstream: UpstreamOptions): boolean => {
+  if (url.hostname !== upstream.domain) {
+    return false;
+  }
+
+  if (url.port !== "") {
+    return false;
+  }
+
+  if (upstream.protocol === undefined && url.protocol !== "https:") {
+    return false;
+  }
+
+  if (
+    upstream.protocol !== undefined &&
+    `${upstream.protocol}:` !== url.protocol
+  ) {
+    return false;
+  }
+  return true;
+};
