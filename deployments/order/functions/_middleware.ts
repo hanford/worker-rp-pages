2df71b19fcd0be1f4da9a186471ff87bde42336e
Jack Hanford <jackhanford@gmail.com>
2022-08-04 11:51:31 -0700

    test order fn

---
+++ deployments/order/functions/_middleware.ts
@@ -0,0 +1,32 @@
+import { decode } from "next-static-manifest/dist/utils";
+import encodedManifest from "./next-static-manifest.json";
+
+const manifest = decode(JSON.stringify(encodedManifest));
+
+const dynamicRouteMiddleware = async ({ next, request }) => {
+  const url = new URL(request.url);
+
+  if (url.pathname.startsWith("/order")) {
+    url.pathname = url.pathname.replace("/order", "");
+  }
+
+  const matchingRoute = manifest.find((k) => {
+    return k.dynamic
+      ? url.pathname.match(new RegExp(k.regex))
+      : k.src === url.pathname;
+  });
+
+  if (matchingRoute) {
+    url.pathname = matchingRoute.src;
+  }
+
+  console.log({ url });
+
+  const response = await next(new Request(url.href));
+
+  response.headers.set("_middleware", true);
+
+  return response;
+};
+
+export const onRequest = [dynamicRouteMiddleware];
